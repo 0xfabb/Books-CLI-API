087b5ec36618ec9908c2f1a06959d76cb617dad3
0xfabb <vanshdev2305@gmail.com>
2026-03-19 20:00:06 +0530

    feat: implemented basic functions for creating a new book and getting all books from file

--- book.go
+++ book.go
@@ -1,17 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
 
-var book = make(map[string]string)
+func createBook(book Book) {
+	var books []Book
 
-func createBook() {
-	book["name"] = "First Book"
-	book["name2"] = "Second Book"
-}
+	file, err := os.Open("books.json")
+	if err == nil {
+		defer file.Close()
+		data, _ := io.ReadAll(file)
 
-func getAllBooks() {
-	for k, v := range book {
-		fmt.Println(k, v)
+		if len(data) > 0 {
+			err = json.Unmarshal(data, &books)
+			if err != nil {
+				fmt.Println("Error reading existing books:", err)
+				return
+			}
+		}
 	}
+
+	books = append(books, book)
+
+	updatedJSON, err := json.MarshalIndent(books, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshaling:", err)
+		return
+	}
+
+	err = os.WriteFile("books.json", updatedJSON, 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		return
+	}
+
+	fmt.Println("Book added successfully")
 }
 
+func getBooks() (books []Book) {
+
+	file, err := os.OpenFile("./books.json", os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Println("There is some error in getting books : ", err)
+	}
+	defer file.Close()
+
+	bookBytes, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Cannot read file because of this error : ", err)
+	}
+	errors := json.Unmarshal(bookBytes, &books)
+	if errors != nil {
+		fmt.Println("Can't unmarshal the bookBytes : ", errors)
+	}
+	return books
+}
--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-type newBook struct {
+type Book struct {
 	Id       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
@@ -13,36 +10,13 @@ type newBook struct {
 	Imageurl string `json:"image_url"`
 }
 
-type Order struct {
-	id        string
-	amount    uint64
-	status    string
-	createdAt time.Time
-}
-
 func main() {
-	num := 5
-	fmt.Println("Number before change is - ", num)
-	var nums = make([]int, 23)
-	fmt.Println(cap(nums))
-	createBook()
-	getAllBooks()
-	fmt.Println("Address of num is - ", &num)
-	changeNum(&num)
-	fmt.Println("Number after change is - ", num)
-
-	order := Order{
-		id:        "1",
-		amount:    43,
-		status:    "delivered",
-		createdAt: time.Now(),
+	// genBook := Book{"12", "firstbook1", "author1", "2201", ""}
+	// createBook(genBook)
+	books := getBooks()
+	for i := range books {
+		fmt.Println(books[0].Title)
+		i++
 	}
-
-	fmt.Println(order.status)
-	fmt.Println(order.createdAt.Date())
-
-}
-
-func changeNum(num *int) {
-	*num = 7
+	fmt.Println(books)
 }
